3d21e85be3a97ed32f0a2b3770e044006eafb872
Your Name <dangtohoang@gmail.com>
2020-05-22 00:01:38 +0700

    Laravel_Todo

---
+++ Laravel_Todo/resources/views/welcome.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Thông tin cá nhân</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <ul>
+            <li>Họ và tên: Đặng Tô Hoàng</li>
+            <li>Năm sinh: 1998</li>
+            <li>Trường đang theo học: Kinh Tế - Kỹ Thuật Công Nghiệp</li>
+            <li>Mục tiêu sau khi học xong khóa Laravel : Kiếm Việc làm, kiếm tiền tiêu</li>
+        </ul>
+    </body>
+</html>
